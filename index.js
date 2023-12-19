91e2ca0aa063a853fcce80bc20f29c82c5339307
Arnold-In-Tech <g16a7782@gmail.com>
2023-12-20 02:19:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+function findMatching(inArray, string){
+
+    return inArray.filter(function (drivername) {
+        return drivername.toUpperCase() === string.toUpperCase();
+    });
+
+}
+
+
+function fuzzyMatch(inArray, prefix){
+
+    return inArray.filter(function (drivername) {
+        return drivername.slice(0,prefix.length) === prefix;
+    });
+
+}
+
+
+function matchName(inArray, string){
+
+    return inArray.filter(function( element){
+        return element.name === string;
+    })
+
+}
